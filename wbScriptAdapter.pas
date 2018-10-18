9edd5eb19a79febfcaca3c9e9c8c3e09a8859d1c
eFrysTon <github@thorsten.engler.id.au>
2018-10-18 22:55:51 +1000

    added wbFormIDErrorCheckLock and wbFormIDErrorCheckUnlock (script accessible)

--- wbScriptAdapter.pas
+++ wbScriptAdapter.pas
@@ -1774,6 +1774,15 @@ begin
     wbLocalizationHandler.GetStringsFromFile(string(Args.Values[0]), TStrings(V2O(Args.Values[1])));
 end;
 
+procedure Misc_wbFormIDErrorCheckLock(var Value: Variant; Args: TJvInterpreterArgs);
+begin
+  Value := wbFormIDErrorCheckLock;
+end;
+
+procedure Misc_wbFormIDErrorCheckUnlock(var Value: Variant; Args: TJvInterpreterArgs);
+begin
+  Value := wbFormIDErrorCheckUnlock;
+end;
 
 procedure RegisterJvInterpreterAdapter(JvInterpreterAdapter: TJvInterpreterAdapter);
 begin
@@ -2080,6 +2089,8 @@ begin
     AddFunction(cUnit, 'wbNormalizeResourceName', Misc_wbNormalizeResourceName, 2, [varEmpty, varEmpty], varEmpty);
     AddFunction(cUnit, 'wbStringListInString', Misc_wbStringListInString, 2, [varEmpty, varEmpty], varEmpty);
     AddFunction(cUnit, 'LocalizationGetStringsFromFile', Misc_LocalizationGetStringsFromFile, 2, [varEmpty, varEmpty], varEmpty);
+    AddFunction(cUnit, 'wbFormIDErrorCheckLock', Misc_wbFormIDErrorCheckLock, 0, [], varEmpty);
+    AddFunction(cUnit, 'wbFormIDErrorCheckUnlock', Misc_wbFormIDErrorCheckUnlock, 0, [], varEmpty);
   end;
 end;
 
